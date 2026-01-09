483f4f75b595931e412387111160ecd25fde30a7
Ezinne Elele <eleleezinne@gmail.com>
2026-01-09 12:46:05 +0100

    deleted files

--- src/services/auditService.js
+++
@@ -1,9 +0,0 @@
-import api from './api';
-
-export const auditService = {
-  async getAuditLogs(params = {}) {
-    const response = await api.get('/api/audit', { params });
-    return response.data;
-  },
-};
-
